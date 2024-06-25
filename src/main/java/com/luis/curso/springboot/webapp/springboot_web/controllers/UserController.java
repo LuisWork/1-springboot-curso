302348b546726fc97bd870f581e290578c0f95f0
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-25 10:16:56 -0400

    Pasando datos a la vista con Map

--- src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.luis.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/details")
-    public String details(Model model) {
+    @GetMapping("/details-model")
+    public String detailsModel(Model model) {
         model.addAttribute("title", "Hello World Spring Boot");
+        model.addAttribute("subtitle", "Pasando datos a la vista con Model");
         model.addAttribute("name", "Luis");
         model.addAttribute("lastname", "Zambrano");
-        return "details";
+        return "details-model";
+    }
+
+    @GetMapping("/details-map")
+    public String detailsMap(Map<String, Object> model) {
+        model.put("title", "Hello World Spring Boot");
+        model.put("subtitle", "Pasando datos a la vista con Map");
+        model.put("name", "Luis");
+        model.put("lastname", "Zambrano");
+        return "details-map";
     }
 
     
