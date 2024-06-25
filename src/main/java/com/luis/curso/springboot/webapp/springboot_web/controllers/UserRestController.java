73b2ec92016362e3115e098270288c6ad1318292
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-25 10:28:07 -0400

    Definiendo una ruta base en el controlador con RequestMapping

--- src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -4,10 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
+
+    //@RequestMapping(path="/details-rest", method =  RequestMethod.GET)
 
     @GetMapping("/details-rest")
     public Map<String, Object> detailsModel() {
