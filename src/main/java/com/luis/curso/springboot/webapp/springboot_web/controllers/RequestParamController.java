834722a9b354b1a98f6a03fc73b7ceed1f2b9c47
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-25 15:53:47 -0400

    Parametros usando HttpServletRequest

--- src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
+++ src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.luis.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 import com.luis.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/api/param")
@@ -25,6 +27,18 @@ public class RequestParamController {
         ParamMixDto params = new ParamMixDto();
         params.setMessage(text);
         params.setCode(code);
+        return params;
+    }
+
+    @GetMapping("/request")
+    public ParamMixDto bar(HttpServletRequest request) {
+        Integer code = 10;
+        try{
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {}
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
         return params;
     }
     
