d01b36f7f5408f3bec390afb8b19a35e115fd01c
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-25 16:12:04 -0400

    Metodo POST create con @RequestBody

--- src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/luis/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luis.curso.springboot.webapp.springboot_web.models.User;
 import com.luis.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 
@@ -28,6 +31,11 @@ public class PathVariableController {
         json.put("product", product);
         json.put("id", id);
         return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
     
 
